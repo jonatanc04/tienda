a3a6982ae8e54f0bdfafa84da648ac8d49881e64
jonatanc04 <jcorcolesg98@gmail.com>
2024-01-31 12:58:16 +0100

    inicio imagenes casa

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,5 +3,13 @@
 @section('titulo', 'A&T Nutrition')
 
 @section('contenido')
-  <p>Hola</p>
+  <div class="inicio-container">
+    <div class="img-container">
+      <img src={{asset("img/left.jpg")}} class="image-product" />
+    </div>
+    <div class="text-container"></div>
+    <div class="img-container">
+      <img src={{asset("img/right.jpg")}} class="image-product" />
+    </div>
+  </div>
 @endsection
