a0878cca2388436e49f2ecd283cb85cfac8d0af2
jonatanc04 <jcorcolesg98@gmail.com>
2024-02-05 18:50:50 +0100

    clase carrito imagen y cantidad prod

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -6,7 +6,12 @@
     <a href="{{ route('inicio') }}"><p>Inicio</p></a>
     @if(auth()->check())
       <a href="{{ route('tienda.index') }}"><p>Productos</p></a>
-      <a href="{{ route('inicio') }}"><p>Carrito</p></a>
+      <a href="{{ route('inicio') }}">
+        <div class="carrito-button">
+          <img src={{asset("img/carrito.png")}} class="image-product" />
+          <div class="cant-prod">1</div>
+        </div>
+      </a>
       @if(auth()->user()->rol === 'admin')
         <a href="{{ route('user.index') }}"><p>Gestión</p></a>
       @endif
